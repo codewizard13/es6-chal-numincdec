db4c45032d93fca5e8b2bec2205ca0ce0195ba03
Eric L. Hepperle <codewizard13@gmail.com>
2021-12-17 13:09:51 -0600

    MOVE container and form to main.php

--- inc/main.php
+++ inc/main.php
@@ -1,7 +1,40 @@
 <?php // main.php ?>
 
 <section class="instructions">
-<p><span class="def-label">Purpose:</span> Build form with plus and minus buttons that increment/decrement the number displayed in a field by one.</p>
+<h3>Purpose:</h3>
+<p>Build form with plus and minus buttons that increment/decrement the number displayed in a field by one.</p>
+<h3>Requirements Given:</h3>
+
+<div class="container">
+<!--
+1. span full height and width
+2. 20 px padding
+3. 5px border red
+
+-->
+
+<div class="form">
+<!--
+1. center vertically and horiz
+2. 2px blue bord
+3. 15px padding
+-->
+<button class="minus" id="minus">
+-
+</button>
+
+<input class="counter" id="counter" >
+
+
+<button class="plus" id="plus">
++
+</button>
+
+</div><!--/form -->
+
+</div><!--/container -->
+
+
 
 
 </section><!--/instructions -->

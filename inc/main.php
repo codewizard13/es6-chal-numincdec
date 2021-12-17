c38ef9840c444e6bde12a92d078e8b84a37df132
Eric L. Hepperle <codewizard13@gmail.com>
2021-12-17 12:23:28 -0600

    UPDATE main.php: Begin writing html

---
+++ inc/main.php
@@ -0,0 +1,8 @@
+<?php // main.php ?>
+
+<section class="instructions">
+<p><span class="def-label">Purpose:</span> Build form with plus and minus buttons that increment/decrement the number displayed in a field by one.</p>
+
+
+</section><!--/instructions -->
+

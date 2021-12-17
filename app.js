d9278fe1a4030e71025aaae1ccc649c3eddd09ba
Eric L. Hepperle <codewizard13@gmail.com>
2021-12-17 11:46:30 -0600

    UPDATE app.js: Add working js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hi')
+var minus_btn = document.getElementById('minus');
+var plus_btn = document.getElementById('plus');
+
+const counter = document.querySelector('.counter');
+counter.value = 0;
+
+minus_btn.addEventListener("click", changeNum, false);
+
+plus_btn.addEventListener("click", changeNum, false);
+
+//console.log(minus_btn);
+
+
+function changeNum (e) {
+	//console.log(e)
+
+  if ( e.target == minus_btn) {
+  	console.log('minus button clicked');
+  	counter.value--;
+  } else if ( e.target == plus_btn) {
+  	console.log('plus button clicked');
+  	counter.value++;
+  }
+  
+}
